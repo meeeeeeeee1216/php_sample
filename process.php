8dab631cc4ac1f97832cd62be47ad1a51a515228
meeeeeeeee1216 <211926725+meeeeeeeee1216@users.noreply.github.com>
2025-10-30 11:45:36 +0900

    バリテーションチェック追加

--- process.php
+++ process.php
@@ -1,5 +1,17 @@
 <?php
     $form = $_GET;
-    
-    
+    $main_url = "Location: http://localhost:" . $_SERVER['SERVER_PORT'] ."/views/main/?";
+    $name = false;
+    $email = false;
+    //名前30、メール50
+    if(mb_strlen($form["name"])> 30){
+        $name = true;
+    } 
+    if(mb_strlen($form["email"]) > 50){
+        $email = true;
+    }
+    if($name || $email){
+        header($main_url . "name='". $name . "'email='" . $email ."'");
+        exit()
+    }
 ?>
--- views/main.php
+++ views/main.php
@@ -1,6 +1,19 @@
 <?php include("head.tpl.php"); ?>
+
 <body>
     <h1>PHPサンプルページ</h1>
+
+    <?php
+        // 名前がおかしい
+        if($_GET["name"] == "true"){
+            echo "名前は30文字以内で入力してください";
+        }
+        if($_GET["email"] == "true"){
+            echo "メールは50文字以内で入力してください"
+        }
+    ?>
+
+
     <form action="check.php" method="get">
         <label for="name">名前:</label>
         <input type="text" id="name" name="name" required>
